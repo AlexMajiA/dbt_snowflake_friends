d7e8674de19ab8a32ef86ce3fa6ad65098ef7d91
Alejandro Martínez Jiménez <amj.parrales@gmail.com>
2025-11-16 16:35:01 +0000

    transformaciones sencillas de STG para dialogs y emotions

--- models/STG_RAW__RAW_EMOTIONS.sql
+++ models/STG_RAW__RAW_EMOTIONS.sql
@@ -12,14 +12,22 @@ WITH raw_emotions_source AS (
 
 raw_emotions_cleaned AS (
     SELECT
-        SEASON ,
-        EPISODE ,
-        SCENE ,
-        UTTERANCE ,
-        EMOTION ,
-        SPEAKER 
+        cast(SEASON as INTEGER) as SEASON,
+        cast(EPISODE as INTEGER) as EPISODE,
+        cast(SCENE as INTEGER) as SCENE,
+        cast(UTTERANCE as INTEGER) as UTTERANCE,
+        trim(EMOTION) as EMOTION,
+        trim(SPEAKER)as SPEAKER,
+
+        CURRENT_TIMESTAMP() as processed_at
 
     FROM raw_emotions_source
+        WHERE SEASON IS NOT NULL
+            AND EPISODE IS NOT NULL
+            AND SCENE IS NOT NULL
+            AND UTTERANCE IS NOT NULL
+            AND EMOTION IS NOT NULL
+            
     )
 
 SELECT * FROM raw_emotions_cleaned
