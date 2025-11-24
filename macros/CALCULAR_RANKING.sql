478f1037c88b48af14ee21387064ac9f6088b44c
Alejandro Martínez Jiménez <amj.parrales@gmail.com>
2025-11-24 09:12:37 +0000

    Macro para ranking, según el parámetro que indiques

---
+++ macros/CALCULAR_RANKING.sql
@@ -0,0 +1,16 @@
+{% macro calcular_ranking(metric) %}
+    case 
+        when {{ metric }} is null then null
+        else rank() over (order by {{ metric }} desc)
+    end
+{% endmacro %}
+
+
+
+
+--NOTAS:
+--Implementé una macro en dbt llamada calcular_ranking() que permite reutilizar
+--la lógica de ranking en distintas métricas (votes, stars, views).
+--La macro recibe el nombre de la métrica como parámetro y genera el SQL
+--correspondiente usando Jinja. Esto evita duplicar código y hace que la fact table
+--sea más limpia y configurable.”
